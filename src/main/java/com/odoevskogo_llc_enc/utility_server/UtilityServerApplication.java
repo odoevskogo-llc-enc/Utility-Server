7e59ee48b09da92d86f4c55bc95b31734dbdfdca
alex-prus <alex.prus.by@gmail.com>
2019-08-06 16:28:41 +0300

    Initial Spring application

---
+++ src/main/java/com/odoevskogo_llc_enc/utility_server/UtilityServerApplication.java
@@ -0,0 +1,13 @@
+package com.odoevskogo_llc_enc.utility_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityServerApplication.class, args);
+	}
+
+}
