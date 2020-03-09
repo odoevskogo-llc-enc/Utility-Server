331c3db84e9e940f5807d08e0202642624de9e6e
alex-prus <alex.prus.by@yandex.ru>
2020-03-09 23:40:54 +0300

    Added index page

--- src/main/java/com/odoevskogo_llc_enc/utility_server/AppController.java
+++ src/main/java/com/odoevskogo_llc_enc/utility_server/AppController.java
@@ -1,12 +1,12 @@
 package com.odoevskogo_llc_enc.utility_server;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AppController {
     @GetMapping("/")
     public String hello() {
-        return "Hello";
+        return "index";
     }
 }
